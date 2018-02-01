6e675e3c81357f93dd679f5b2c75d8ed09a026de
Leon Yang <leon.gh.yang@gmail.com>
2018-02-01 23:26:53 +0000

    Update benchmark script to use payload from s3

--- bench-new.py
+++ bench-new.py
@@ -5,40 +5,51 @@ parser.add_argument('--num-images', default=1, type=int, help='Number of copies 
 parser.add_argument('--num-requests', default=1, type=int, help='Number of times to send request.')
 parser.add_argument('service', type=str, help='{aws,ol,ow}')
 
-def main(args):
-	with open('texture.tiff', 'rb') as img:
-		data = base64.b64encode(img.read())
-		payload = json.dumps({'Records':[{'data':data, 'format':'tiff'} for _ in range(args.num_images)]})
+record = json.loads('''
+{
+  "s3": {
+    "bucket": {
+      "name": "lambda-resize-image"
+    },
+    "object": {
+      "key": "texture.tiff"
+    }
+  }
+}
+''')
 
-	if args.service == 'aws':
-		aws(payload, args.num_requests)
-	elif args.service == 'ol':
-		ol(payload, args.num_requests)
-	else:
-		print('invalid service: %s' % args.service)
-		sys.exit(1)
+def main(args):
+    payload = json.dumps({'Records': [record for _ in range(args.num_images)]})
+
+    if args.service == 'aws':
+        aws(payload, args.num_requests)
+    elif args.service == 'ol':
+        ol(payload, args.num_requests)
+    else:
+        print('invalid service: %s' % args.service)
+        sys.exit(1)
 
 def aws(payload, n_requests):
-	import boto3
-	client = boto3.client('lambda')
-	print('latency lambda_duration')
-	for i in xrange(n_requests):
-		start = time.time()
-		response = client.invoke(
-			FunctionName='resize',
-			Payload=payload
-		)
-		elapsed = (time.time() - start) * 1000
-		output = json.loads(response['Payload'].read())
-		print('%.3f %.3f' % (elapsed, output['duration']))
+    import boto3
+    client = boto3.client('lambda')
+    print('latency lambda_duration')
+    for i in xrange(n_requests):
+        start = time.time()
+        response = client.invoke(
+            FunctionName='resize',
+            Payload=payload
+        )
+        elapsed = (time.time() - start) * 1000
+        output = json.loads(response['Payload'].read())
+        print('%.3f %.3f' % (elapsed, output['duration']))
 
 def ol(payload, n_requests):
-	print('latency lambda_duration')
-	for i in xrange(n_requests):
-		start = time.time()
-		r = requests.post('http://localhost:8080/runLambda/ol-resize', data=payload)
-		elapsed = (time.time() - start) * 1000
-		print('%.3f %.3f' % (elapsed, r.json()['duration']))
+    print('latency lambda_duration')
+    for i in xrange(n_requests):
+        start = time.time()
+        r = requests.post('http://localhost:8080/runLambda/ol-resize', data=payload)
+        elapsed = (time.time() - start) * 1000
+        print('%.3f %.3f' % (elapsed, r.json()['duration']))
 
 if __name__ == '__main__':
-	main(parser.parse_args())
+    main(parser.parse_args())
