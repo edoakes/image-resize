a0aacf6c55bb3ab0d9c432d9e5f1c8d7a5dcb99e
Leon Yang <leon.gh.yang@gmail.com>
2018-02-01 05:02:35 +0000

    Changes:
    1. Use libraries for AWS Lambda Linux distribution (Fedora)
    2. Forget about S3, now send base64 encoded image in payload

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-zip -r9 resize.zip venv/lib/python2.7/site-packages/*
+zip -r9 resize.zip lib/*
 zip -g resize.zip resize.py
--- resize.py
+++ resize.py
@@ -1,25 +1,27 @@
 from __future__ import print_function
-import boto3
-import os
+import base64
+from cStringIO import StringIO
+import time
 import sys
-import uuid
+sys.path.insert(0,'./lib')
+import boto3
 from PIL import Image
-import PIL.Image
      
 s3_client = boto3.client('s3')
      
-def resize_image(image_path, resized_path):
-    with Image.open(image_path) as image:
+def resize_image(record):
+    decoded = base64.b64decode(record['data'])
+    stream = StringIO(decoded)
+    with Image.open(stream) as image:
         image.thumbnail(tuple(x / 2 for x in image.size))
-        image.save(resized_path)
+        output = StringIO()
+        image.save(output, format=record['format'])
+        return base64.b64encode(output.getvalue())
      
 def handler(event, context):
+    start = time.time()
+    output = {'resized': []}
     for record in event['Records']:
-        bucket = record['s3']['bucket']['name']
-        key = record['s3']['object']['key'] 
-        download_path = '/tmp/{}{}'.format(uuid.uuid4(), key)
-        upload_path = '/tmp/resized-{}'.format(key)
-        
-        s3_client.download_file(bucket, key, download_path)
-        resize_image(download_path, upload_path)
-        s3_client.upload_file(upload_path, '{}resized'.format(bucket), key)
+        output['resized'].append(resize_image(record))
+    output['duration'] = (time.time() - start) * 1000
+    return output
