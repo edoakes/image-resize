5257c0cf466d3e51a0ae6541b47cdfa649c3dd80
Leon Yang <leon.gh.yang@gmail.com>
2018-02-01 23:14:30 +0000

    Use s3 for handler payload

--- resize.py
+++ resize.py
@@ -1,24 +1,26 @@
 from __future__ import print_function
-import base64
-from cStringIO import StringIO
 import time
+import uuid
 import sys
 sys.path.insert(0,'./lib')
 from PIL import Image
-     
-def resize_image(record):
-    decoded = base64.b64decode(record['data'])
-    stream = StringIO(decoded)
-    with Image.open(stream) as image:
+import boto3
+
+def resize_image(image_path, resized_path):
+    with Image.open(image_path) as image:
         image.thumbnail(tuple(x / 2 for x in image.size))
-        output = StringIO()
-        image.save(output, format=record['format'])
-        return base64.b64encode(output.getvalue())
-     
+        image.save(resized_path)
+
 def handler(event, context):
     start = time.time()
-    output = {'resized': []}
+    s3_client = boto3.client('s3')
     for record in event['Records']:
-        output['resized'].append(resize_image(record))
-    output['duration'] = (time.time() - start) * 1000
-    return output
+        bucket = record['s3']['bucket']['name']
+        key = record['s3']['object']['key']
+        download_path = '/tmp/{}{}'.format(uuid.uuid4(), key)
+        upload_path = '/tmp/resized-{}'.format(key)
+
+        s3_client.download_file(bucket, key, download_path)
+        resize_image(download_path, upload_path)
+        s3_client.upload_file(upload_path, '{}-resized'.format(bucket), key)
+    return {'duration': (time.time() - start) * 1000}
